4fe1fe9c59f8be55a1c48c3a2a71484c8ed8c758
yashv7982 <115337701+yashv7982@users.noreply.github.com>
2024-10-11 10:53:19 +0530

    Update App.jsx

--- frotend/src/App.jsx
+++ frotend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import{Route,Routes} from "react-router-dom"
-import Home from './pages/home'
+import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
