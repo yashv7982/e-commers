81299d5720c801eb5c1019bf4cafa2dc9c0d1325
yashv7982 <115337701+yashv7982@users.noreply.github.com>
2024-10-11 12:57:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ import CartRouter from "./Routes/cartRoutes.js"
 const app = express()
 const port = process.env.PORT || 4000
 
-
+const allowedOrigins = [
+  'https://e-commers-frotend.vercel.app', // Frontend Domain
+  'https://e-commers-admin-two.vercel.app', // Admin Panel Domain
+];
 connectDB()
 connectCloudinary()
 
@@ -18,9 +21,15 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors({
-    origin: 'https://e-commers-frotend.vercel.app',  // Replace with your frontend domain
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
+    credentials: true,
 }));
 
 // api ends
